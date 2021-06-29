54aaa5394bbcee132e8cca3d337fa658010ea494
ahstevens11 <austin.cards20@gmail.com>
2021-06-29 15:05:01 -0500

    More DB work

--- db/index.js
+++ db/index.js
@@ -21,7 +21,71 @@ async function createUser({username, password}) {
     }
 }
 
+async function createCard({
+    card_title,
+    description,
+    price,
+    card_img,
+    view_count
+}) {
+    try {
+        const {rows: [card]} = await client.query(`
+        INSERT INTO cards(card_title, description, price, card_img, view_count)
+        VALUES($1, $2, $3, $4, $5)
+        RETURNING *;
+        `, [card_title, description, price, card_img, view_count])
+
+        return card;
+    } catch (error) {
+        throw error
+    }
+}
+
+async function createTags(tag_content) {
+    try {
+        const {rows: [tags]} = await client.query(`
+        INSERT INTO tags(tag_content)
+        VALUES ($1)
+        RETURNING *;
+        `, [tag_content])
+
+        return tags;
+    } catch (error) {
+        throw error
+    }
+}
+
+async function getTagByContent(tag_content) {
+    try {
+        const {rows: [tag]} = await client.query(`
+        SELECT * FROM tags
+        WHERE tag_content = '${tag_content}'
+        `)
+
+        return tag
+    } catch (error) {
+        throw error
+    }
+}
+
+async function createCardTag(cardId, tagId) {
+    try {
+        await client.query(`
+        INSERT INTO card_tags("cardId", "tagId")
+        VALUES ($1, $2)
+        ON CONFLICT ("cardId", "tagId") DO NOTHING
+        `, [cardId, tagId])
+
+    } catch (error) {
+        throw error
+    }
+}
+
 module.exports = {
     client,
-    createUser
+    createUser,
+    createCard,
+    createTags,
+    getTagByContent,
+    createCardTag
 }
--- db/init_db.js
+++ db/init_db.js
@@ -1,6 +1,10 @@
 const {
     client,
-    createUser
+    createUser,
+    createCard,
+    createTags,
+    getTagByContent,
+    createCardTag
 } = require("./index");
 
 async function buildTables() {
@@ -10,7 +14,9 @@ async function buildTables() {
         console.log("starting to drop tables");
         //DROP TABLE will go in here
         await client.query(`
+        DROP TABLE IF EXISTS img;
         DROP TABLE IF EXISTS cart;
+        DROP TABLE IF EXISTS card_tags;
         DROP TABLE IF EXISTS tags;
         DROP TABLE IF EXISTS cards;
         DROP TABLE IF EXISTS users;
@@ -29,12 +35,19 @@ async function buildTables() {
                 card_title VARCHAR(255) NOT NULL,
                 description VARCHAR(255) NOT NULL,
                 price INT NULL,
+                view_count INT NULL,
                 card_img BYTEA NULL,
                 creation_date DATE NOT NULL DEFAULT CURRENT_DATE
             );
             CREATE TABLE tags(
                 ID SERIAL PRIMARY KEY,
                 tag_content VARCHAR(255) UNIQUE NOT NULL
+            );
+            CREATE TABLE card_tags(
+                ID SERIAL PRIMARY KEY,
+                "cardId" INT REFERENCES cards(ID) NOT NULL,
+                "tagId" INT REFERENCES tags(ID) NOT NULL,
+                UNIQUE ("cardId", "tagId")
             );
             CREATE TABLE cart(
                 ID SERIAL PRIMARY KEY,
@@ -44,7 +57,7 @@ async function buildTables() {
             );
             CREATE TABLE img(
                 ID SERIAL PRIMARY KEY,
-                "cardId" INT REFENCES cards(ID),
+                "cardId" INT REFERENCES cards(ID),
                 name TEXT,
                 img TEXT
             )
@@ -60,20 +73,29 @@ async function populateInitialData() {
         await createUser({
             username: "Eman",
             password: "CodingGod"
+        });
+        await createUser({
+            username: "Astevens14",
+            password: "GoColts20"
         })
         console.log("finished creating intial users")
 
+        console.log("creating new card listing")
         await createCard({
             card_title: "Lebron James Topps Rookie",
             description: "mint condition LBJ rookie card",
-            price: "$1200",
-            card_img: "",         
+            price: "1200",
+            view_count: "25",
+            card_img: "https://th.bing.com/th/id/R4ec8fb7fe602023e56a0950eab2b69c6?rik=fF8DNf3Q%2bOblsQ&pid=ImgRaw"         
         })
+        console.log("finished creating new card listing")
 
         console.log("creating initial tags")
         await createTags("Pokemon")
         await createTags("Basketball")
         console.log("finished creating intial tags")
+
+        await createCardTag(1, 1)
     } catch(error) {
         throw error
     }
