0a077dfcb8bb2a17600d5871023832b3f9370995
Emmanuel Ejim <emmanueleejim@gmail.com>
2021-07-01 12:59:21 -0500

    Big Style Boost ~Eman

--- src/App.js
+++ src/App.js
@@ -1,8 +1,18 @@
+import Navbar from "./components/Navbar/Navbar.js"
+import HomeBanner from "./components/Carousel/SlideBanner.js"
+import LeftNavBar from "./components/SideBar/SideBar.js";
+
 import "./App.css";
+
 
 function App() {
   return (
-    <h1>Hello World - CardEx</h1>
+    <div className="appContainer">
+      <Navbar />
+      <HomeBanner />
+      <LeftNavBar />
+      {/* <h1>Hello World - CardEx</h1> */}
+    </div>
   );
 }
 
--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -1,6 +1,45 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
-
+// import ReactDOM from 'react-dom';
+import { FcSettings } from 'react-icons/fc'
 
 import "./Navbar.css";
 
+const Navbar = (props) => {
+    console.log(props)
+    return (
+			<div className="navbarContainer">
+				<ul class="nav">
+						{/* <li id="settings">
+							<a href="#"> <FcSettings /> </a>
+						</li> */}
+						<li>
+							<a href="#">CardEx US</a>
+						</li>
+						<li>
+							<a href="#">Sell Cards</a>
+						</li>
+						<li id="search">
+								<form action="" method="get">
+									<input type="text" name="search_text" id="search_text" placeholder="Search"/>
+									<input type="button" name="search_button" id="search_button"/>
+								</form>
+						</li>
+						<li id="options">
+								<a href="#">Login/Signup</a>
+								<ul class="subnav">
+									<li><a href="#">User Profile</a></li>
+									<li><a href="#">Settings</a></li>
+									<li><a href="#">Admin Management</a></li>
+								</ul>
+						</li>
+						<li>
+							<a href="#">Cart</a>
+						</li>
+				</ul>
+
+				<script src="prefixfree-1.0.7.js" type="text/javascript"></script>
+			</div>
+    );
+}
+
+export default Navbar;
