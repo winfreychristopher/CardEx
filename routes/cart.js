f6273c08be43f7a4a3c42fb4c75db29cfba157eb
winfreychristopher <winfreychristopher@Chris-PC.localdomain>
2021-07-07 18:30:10 -0500

    fixed routes and token error

--- routes/cart.js
+++ routes/cart.js
@@ -3,11 +3,22 @@ const cartRouter = express.Router();
 const jwt = require("jsonwebtoken");
 const { JWT_SECRET } = process.env;
 const { requireUser } = require("./utils");
+const { getCartByUserId, getCardsById, addCardToCart } = require("../db");
 
 cartRouter.use((req, res, next) => {
   console.log("A request is being made to /cart");
 
   next();
+});
+
+cartRouter.get("/:userId", requireUser, async (req, res, next) => {
+  const { userId } = req.params;
+  try {
+    const cart = await getCartByUserId(userId);
+    res.send(cart);
+  } catch (error) {
+    throw error;
+  }
 });
 
 module.exports = cartRouter;
