6550bdb8944d184e82b7ab30f4f47642c94f248b
Emmanuel Ejim <emmanueleejim@gmail.com>
2021-07-07 19:16:59 -0500

    login functionality should be ~Eman

--- src/components/LoginPage/Login.js
+++ src/components/LoginPage/Login.js
@@ -1,6 +1,6 @@
 import React, { useState } from 'react';
 import { useLocation } from 'react-router-dom';
-import { userRegister } from '../../api/index';
+import { userRegister, userLogin } from '../../api/index';
 
 import "./Login.css";
 
@@ -15,24 +15,55 @@ const LoginPage  = () => {
   const userForms = document.getElementById('user_options-forms');
   const [username, setUsername] = useState("");
   const [password, setPassword] = useState("");
+  const [repassword, setRepassword] = useState("");
   const [email, setEmail] = useState("");
+  const [errMsgText, setErrMsgText] = useState("");
+  const [msgColor, setMsgColor] = useState("");
+
+  // let msgColor;
 
   // const onFormSubmit = async (e) => {
   //   e.preventDefault();
 
   // }
+  
   const SignupUser = async (e, name, pass, mail) => {
-    
+    console.log(repassword, password)
+    e.preventDefault();
+    if (repassword === password) {    
+      try {
+        const res = await userRegister(name, pass, mail);
+        console.log(res);
+        setMsgColor('green')
+        setErrMsgText("Thank you for signing up.");
+        e.preventDefault();
+        
+      } catch (err) {
+        setErrMsgText(err);
+        console.log(err);
+        e.preventDefault();
 
-    try {
-      const res = await userRegister(name, pass);
-      console.log(res);
-      document.getElementById("errMsg").innerHTML = `${"You have Logged in " + name + "."}`;
+      }
+    } else {   
+      setMsgColor('red')
+      setErrMsgText("Passwords Do not Match");
       e.preventDefault();
-      
+    }
+  }
+
+  const LoginUser = async (e, name, pass) => {
+    console.log(name, pass)
+    e.preventDefault();
+    try {
+      const res = await userLogin(name, pass);
+      console.log(res);
+      setMsgColor('green')
+      setErrMsgText("Welcome back " + name);
+      e.preventDefault();
+
     } catch (err) {
-      document.getElementById("errMsg").innerHTML = `${err}`;
-      console.log(err)
+      console.log(err);
+      e.preventDefault();
     }
   }
 
@@ -79,17 +110,25 @@ const LoginPage  = () => {
         <div class="user_options-forms" id="user_options-forms">
           <div class="user_forms-login">
             <h2 class="forms_title">Login</h2>
-            <div id="errMsg">
-       
-            </div>
-            <form class="forms_form">
+            <div id="errMsg" > {errMsgText} </div> 
+
+            {/* The Login Form */}
+            <form class="forms_form" onSubmit={(e) => {LoginUser( e, username, password)}}>
               <fieldset class="forms_fieldset">
                 <div class="forms_field">
-                  <input type="text" class="forms_field-input" required />
-                  <label class="forms_field-label">Email</label>
+                  <input type="text" class="forms_field-input" required 
+                    onChange={(event) => {
+                      setUsername(event.target.value);
+                    }}
+                  />
+                  <label class="forms_field-label">Username</label>
                 </div>
                 <div class="forms_field">
-                  <input type="password" class="forms_field-input" required />
+                  <input type="password" class="forms_field-input" required 
+                    onChange={(event) => {
+                      setPassword(event.target.value);
+                    }}
+                  />
                   <label class="forms_field-label">Password</label>
                 </div>
               </fieldset>
@@ -100,23 +139,21 @@ const LoginPage  = () => {
                 <input
                   type="submit"
                   value="Login"
-                  class="forms_buttons-action"
-                  
+                  class="forms_buttons-action"                 
                 />
               </div>
             </form>
           </div>
           <div class="user_forms-signup">
             <h2 class="forms_title">Sign Up</h2>
-            <div id="errMsg">
-            
-            </div>
+            <div id="errMsg" style={{ color: msgColor }}> {errMsgText} </div>
+
+            {/* The Sign Up Form */}
             <form class="forms_form" onSubmit={(event) => {SignupUser( event, username, password, email)}}>
               <fieldset class="forms_fieldset">
                 <div class="forms_field">
                   <input type="text" class="forms_field-input" required 
-                    onInput={(event) => {
-                      console.log(event.target.value)
+                    onChange={(event) => {
                       setUsername(event.target.value);
                     }} 
                   />
@@ -124,24 +161,26 @@ const LoginPage  = () => {
                 </div>
                 <div class="forms_field">
                   <input type="text" class="forms_field-input" required 
-                    onInput={(event) => {
-                      console.log(event.target.value)
+                    onChange={(event) => {
                       setEmail(event.target.value);
                     }} 
                   />
                   <label class="forms_field-label">Email</label>
                 </div>
                 <div class="forms_field">
-                <label class="forms_field-label">Password</label>
                   <input type="password" class="forms_field-input" required 
-                    onInput={(event) => {
-                      console.log(event.target.value)
+                    onChange={(event) => {
                       setPassword(event.target.value);                     
                     }} 
                   />
+                  <label class="forms_field-label">Password</label>
                 </div>
                 <div class="forms_field">
-                  <input type="password" class="forms_field-input" required />
+                  <input type="password" class="forms_field-input" required
+                    onChange={(e) => {
+                      setRepassword(e.target.value);
+                    }}
+                  />
                   <label class="forms_field-label">Confirm Password</label>
                 </div>
               </fieldset>
@@ -149,8 +188,7 @@ const LoginPage  = () => {
                 <input
                   type="submit"
                   value="Sign up"
-                  class="forms_buttons-action"
-                  
+                  class="forms_buttons-action"                 
                 />
               </div>
             </form>
